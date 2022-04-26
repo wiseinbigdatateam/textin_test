295d9a2bd68f8d09fa1fb9cf20787653e2654087
seoyoung <astakara48@gmail.com>
2022-04-26 09:11:28 +0900

    main

---
+++ main.py
@@ -0,0 +1,7 @@
+from preprocess import preprocess
+from es import from_es
+
+if __name__ == "__main__":
+    ps = preprocess.Preprocessor()
+    keywords = ['경기도', '석촌', '아파트', '부동산', '서울']
+    save_df = ps.clean('mecab', [1,2,3,5], keywords)
